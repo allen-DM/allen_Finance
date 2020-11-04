f2e21ae20853248a8dd089df90f47591dbe37078
WHQ\10807061 <h88129@gmail.com>
2020-11-04 13:17:29 +0800

    增加import config 新增 config使用方法

---
+++ R/RFinance/R/test_configuse.R
@@ -0,0 +1,12 @@
+install.packages("config")
+
+library(config)
+
+config <- config::get(file = "configs/config.yml")
+
+Sys.setenv(R_CONFIG_ACTIVE = "default")
+config::is_active("production")
+config::get("trials")
+
+config$trials
+getwd()
